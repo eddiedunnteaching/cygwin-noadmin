4f3d57d2f9569b19ef95ccfbdb0f940433706dbc
Eddie Dunn <eddie@eddiedunn.com>
2022-09-17 16:45:36 -0400

    more tweaking   Please enter the commit message for your changes. Lines starting

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# cygwin-noadmin
-Install cygwin with packages and without admin on windows
+# Install cygwin with packages and without admin on windows
+
+This is a simple way to install and update a non-admin cygwin installation.
+
+NOTE: If you would like to install `wget` as many of the tutorials tell you as a non-admin you will run into issues with `libusb0` as it requires a driver install that a non-admin cannot perform. You can just "skip" the package install and it will work but I thought it more simple to just use `curl` instead. 
--- install.bat
+++ install.bat
@@ -1,2 +1,4 @@
 curl https://www.cygwin.com/setup-x86_64.exe --output setup-x86_64.exe
-setup-x86_64.exe --no-shortcuts  --no-admin  -D --only-site  --site https://mirrors.rit.edu/cygwin/ --root cygwin64 --remove-packages libusb0  --packages "wget,git" 
+curl https://raw.githubusercontent.com/transcode-open/apt-cyg/master/apt-cyg --output apt-cyg
+setup-x86_64.exe --no-desktop --no-startmenu --no-replaceonreboot  --no-admin --site https://mirrors.rit.edu/cygwin/ --root cygwin64 --packages "git,lynx"  --quiet-mode
+copy apt-cyg cygwin64\bin
