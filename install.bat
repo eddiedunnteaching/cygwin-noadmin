5d6d8c60eeb37cf0a91164af39b94eabbe8a7f25
eddiedunn <eddie@eddiedunn.com>
2022-10-09 12:16:10 -0400

    added ansible and ansible-doc to package list

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 curl https://www.cygwin.com/setup-x86_64.exe --output setup-x86_64.exe
 curl https://raw.githubusercontent.com/transcode-open/apt-cyg/master/apt-cyg --output apt-cyg
-setup-x86_64.exe --no-desktop --no-startmenu --no-replaceonreboot  --no-admin --site https://mirrors.rit.edu/cygwin/ --root cygwin64 --packages "git,lynx"  --quiet-mode
+setup-x86_64.exe --no-desktop --no-startmenu --no-replaceonreboot  --no-admin --site https://mirrors.rit.edu/cygwin/ --root cygwin64 --packages "git,lynx,ansible,ansible-doc"  --quiet-mode
 copy apt-cyg cygwin64\bin
